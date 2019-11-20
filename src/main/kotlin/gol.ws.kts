6739fa2727b49d98fd253ec88b2a223bb198e27e
clojj <jwin1968@gmail.com>
2019-11-20 22:32:31 +0100

    - type definitions in separate sourcefiles seem to have problems with Kotlin workspaces - lambda as values

--- src/main/kotlin/gol.ws.kts
+++ src/main/kotlin/gol.ws.kts
@@ -1,22 +1,74 @@
-import funk.Cell
-import funk.World
+data class Cell(val x: Int, val y: Int) : Comparable<Cell> {
+    override fun compareTo(other: Cell): Int = compareValuesBy(this, other, { it.x }, { it.y })
+}
 
-val c1 = Cell(3, 4)
-println(c1)
+data class World(val cells: Set<Cell>)
 
-val c2 = Cell(3, 5)
+val world = World(setOf(Cell(3, 1), Cell(1, 2), Cell(1, 3), Cell(2, 3)))
 
-println(c2 == c1)
-println(c2 > c1)
+println(world)
 
 
-val world = World(
-    setOf(
-        Cell(3, 1),
-        Cell(1, 2),
-        Cell(1, 3),
-        Cell(2, 3)
+// in 4 Schritten von einer 'Methode' zur Funktion-als-Wert
+
+fun neighbors1(cell: Cell): List<Cell> {
+    val (x, y) = cell
+    return listOf(
+        Cell(x, y + 1),
+        Cell(x, y - 1),
+        Cell(x + 1, y),
+        Cell(x - 1, y),
+        Cell(x + 1, y + 1),
+        Cell(x + 1, y - 1),
+        Cell(x - 1, y + 1),
+        Cell(x - 1, y - 1)
     )
-)
+}
 
-println(world.cells.joinToString(","))
+println(neighbors1(Cell(1, 1)))
+
+fun neighbors2(): (Cell) -> List<Cell> = { (x, y) ->
+    listOf(
+        Cell(x, y + 1),
+        Cell(x, y - 1),
+        Cell(x + 1, y),
+        Cell(x - 1, y),
+        Cell(x + 1, y + 1),
+        Cell(x + 1, y - 1),
+        Cell(x - 1, y + 1),
+        Cell(x - 1, y - 1)
+    )
+}
+
+println(neighbors2()(Cell(1, 1)))
+
+val neighbors3: (Cell) -> List<Cell> = { (x, y) ->
+    listOf(
+        Cell(x, y + 1),
+        Cell(x, y - 1),
+        Cell(x + 1, y),
+        Cell(x - 1, y),
+        Cell(x + 1, y + 1),
+        Cell(x + 1, y - 1),
+        Cell(x - 1, y + 1),
+        Cell(x - 1, y - 1)
+    )
+}
+
+println(neighbors3(Cell(1, 1)))
+
+val neighbors4 = { (x, y): Cell ->
+    listOf(
+        Cell(x, y + 1),
+        Cell(x, y - 1),
+        Cell(x + 1, y),
+        Cell(x - 1, y),
+        Cell(x + 1, y + 1),
+        Cell(x + 1, y - 1),
+        Cell(x - 1, y + 1),
+        Cell(x - 1, y - 1)
+    )
+}
+
+println(neighbors4(Cell(1, 1)))
+
