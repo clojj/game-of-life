5630a890b4f1b765521e8b4bf45b604294d7d683
Joerg Winter <joerg.winter@f-i-solutions-plus.de>
2019-11-25 09:58:34 +0100

    chunked

--- src/main/kotlin/pmap.kt
+++ src/main/kotlin/pmap.kt
@@ -1,20 +1,24 @@
 import kotlinx.coroutines.*
+import java.math.BigInteger
+import java.util.*
 import kotlin.system.measureTimeMillis
 
-val e = 10
-
 suspend fun <A, B> Iterable<A>.pmap(f: suspend (A) -> B): List<B> = coroutineScope {
-    map { async { f(it) } }.awaitAll()
+    map { async(Dispatchers.Default) { f(it) } }.awaitAll()
 }
+
+suspend fun <A, B> Iterable<A>.pmapChunked(f: suspend (A) -> B): List<B> = coroutineScope {
+    val deferred = chunked(5).map { async(Dispatchers.Default) { it.map { f(it) } } }
+    val all = deferred.awaitAll()
+    all.flatten()
+}
+
 fun main(args: Array<String>) = runBlocking {
     val time = measureTimeMillis {
-        val output = (1..1000).pmap {
-            delay(1000)
-            it * 2
+        val output = (1..100).pmapChunked {
+            val nextProbablePrime = BigInteger(1000, Random()).nextProbablePrime()
+            "$it $nextProbablePrime"
         }
-
-        println(output)
     }
-
     println("Total time: $time")
 }
