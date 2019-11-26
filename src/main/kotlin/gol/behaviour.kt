420e11e0bb0751f2a04f36696248b07d765fcbc4
clojj <jwin1968@gmail.com>
2019-11-26 23:26:58 +0100

    neighborCounts

--- src/main/kotlin/gol/behaviour.kt
+++ src/main/kotlin/gol/behaviour.kt
@@ -3,3 +3,11 @@ package gol
 val neighbors = { (x, y): Cell ->
     (-1..1).flatMap { dx -> (-1..1).map { dy -> Cell(x + dx, y + dy) } }.toSet() - Cell(x, y)
 }
+
+val neighborCounts = { world: World ->
+    world.cells
+        .flatMap { cell -> neighbors(cell) }
+        .sorted()
+        .groupBy({ it }, { _ -> Unit })
+        .mapValues { (_, cells) -> cells.size }
+}
--- src/test/kotlin/GolTests.kt
+++ src/test/kotlin/GolTests.kt
@@ -1,15 +1,22 @@
 import gol.Cell
+import gol.World
+import gol.neighborCounts
 import gol.neighbors
 import io.kotlintest.data.forall
+import io.kotlintest.matchers.maps.shouldContainAll
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import io.kotlintest.tables.row
 
 class GolTests : StringSpec({
 
-    val cell = Cell(1, 1)
+    "neighborCounts of a single cell" {
+        val cell = Cell(1, 1)
+        val cellNeighbors = neighbors(cell).map { Pair(it, 1) }.toMap()
+        neighborCounts(World(setOf(cell))) shouldContainAll cellNeighbors
+    }
 
-    "neighbors" {
+    "neighbor cells" {
         forall(
             row(42, 42),
             row(0, 0),
